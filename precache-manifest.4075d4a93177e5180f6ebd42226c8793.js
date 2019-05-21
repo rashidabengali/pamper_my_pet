dc60660098264977d81754997b2dbcf00e951f0f
RashidaBengali <bengali.rashida@gmail.com>
2019-05-21 11:09:15 +1000

    Updates

---
+++ precache-manifest.4075d4a93177e5180f6ebd42226c8793.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8128e104976659985ce7c895a7f4132d",
+    "url": "/pamper_my_pet/index.html"
+  },
+  {
+    "revision": "fa52ce193a5ceecbd307",
+    "url": "/pamper_my_pet/static/css/2.fed7966d.chunk.css"
+  },
+  {
+    "revision": "657599c2adae0851a77c",
+    "url": "/pamper_my_pet/static/css/main.ceea5e36.chunk.css"
+  },
+  {
+    "revision": "fa52ce193a5ceecbd307",
+    "url": "/pamper_my_pet/static/js/2.98dade0c.chunk.js"
+  },
+  {
+    "revision": "657599c2adae0851a77c",
+    "url": "/pamper_my_pet/static/js/main.d083651f.chunk.js"
+  },
+  {
+    "revision": "00fbdd2739e8dd5150cd",
+    "url": "/pamper_my_pet/static/js/runtime~main.6e87f5b0.js"
+  },
+  {
+    "revision": "f7f1f7ba1be24afdcbf77a3e3d928238",
+    "url": "/pamper_my_pet/static/media/Shopping-dog.f7f1f7ba.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pamper_my_pet/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pamper_my_pet/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pamper_my_pet/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pamper_my_pet/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pamper_my_pet/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
